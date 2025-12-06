0a5d707e144729685e18a4774deef27fbefe84f8
Sebastian Mueller <s.mueller@grizzlyweb.de>
2025-12-06 17:13:02 +0100

    Update .config/nvim/lua/custom/plugins/neo-tree.lua

--- dot_config/nvim/lua/custom/plugins/neo-tree.lua
+++ dot_config/nvim/lua/custom/plugins/neo-tree.lua
@@ -17,9 +17,10 @@ return {
 		close_if_last_window = true,
 		filesystem = {
 			filtered_items = {
-				hide_dotfiles = false,
-				hide_gitignored = false,
-				hide_hidden = false,
+				visible = true,
+				-- hide_dotfiles = false,
+				-- hide_gitignored = false,
+				-- hide_hidden = false,
 			},
 			window = {
 				mappings = {
