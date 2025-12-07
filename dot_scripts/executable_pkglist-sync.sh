1904b073e472aa302253f588066cb52245dbf2a2
Sebastian Mueller <s.mueller@grizzlyweb.de>
2025-12-07 16:31:29 +0100

    Add .scripts/pkglist-sync.sh

---
+++ dot_scripts/executable_pkglist-sync.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+JSON="$HOME/.local/share/chezmoi/explicit-packages.json"
+HOST="$(hostnamectl --static)"
+
+[ -f "$JSON" ] || echo "[]" > "$JSON"
+
+pkgs=$(pacman -Qqe)
+
+tmp="$(mktemp)"
+python3 - "$JSON" "$HOST" <<EOF
+import json, sys, datetime, subprocess
+
+f, host = sys.argv[1:]
+today = datetime.date.today().isoformat()
+
+data = json.load(open(f))
+names = {p["name"] for p in data}
+
+# pacman packages
+pkgs = """$pkgs""".split()
+
+for pkg in pkgs:
+    if pkg not in names:
+        data.append({
+            "name": pkg,
+            "allowed_hosts": [host],
+            "installed": today
+        })
+
+json.dump(data, open(f, "w"), indent=2)
+EOF
+
+mv "$tmp" "$JSON"
+
