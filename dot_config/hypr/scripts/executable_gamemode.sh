a6944571b38f2e16450dbb3f85780b95be684d45
Sebastian Mueller <s.mueller@grizzlyweb.de>
2025-12-09 10:41:34 +0100

    Update .config/hypr/scripts/gamemode.sh

--- dot_config/hypr/scripts/executable_gamemode.sh
+++ dot_config/hypr/scripts/executable_gamemode.sh
@@ -5,7 +5,6 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword decoration:fullscreen_opacity 1;\
         keyword decoration:active_opacity 1;\
         keyword decoration:inactive_opacity 1;\
-	keyword windowrule 'opacity 1.0 1.0 override override, class:.*';\
         keyword animations:enabled 0;\
         keyword animation borderangle,1; \
         keyword decoration:shadow:enabled 0;\
@@ -14,7 +13,9 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+	keyword windowrule 'opacity 1.0 1.0 override override, class:.*';
+    "
     hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
     exit
 else
